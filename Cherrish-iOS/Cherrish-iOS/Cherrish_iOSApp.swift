1d935f132dcb015cb61b0e39d5d9c6f260490ab9
yeonee <leenayeon@sookmyung.ac.kr>
2025-12-31 17:22:20 +0900

    setting: #1 프로젝트 추가

---
+++ Cherrish-iOS/Cherrish-iOS/Cherrish_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cherrish_iOSApp.swift
+//  Cherrish-iOS
+//
+//  Created by 이나연 on 12/31/25.
+//
+
+import SwiftUI
+
+@main
+struct Cherrish_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cherrish-iOS/Cherrish-iOS/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Cherrish-iOS
+//
+//  Created by 이나연 on 12/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
