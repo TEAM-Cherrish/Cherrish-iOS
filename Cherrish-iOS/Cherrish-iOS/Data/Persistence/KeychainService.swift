cadc09fdd6089b0843f1550761d855e225daaff2
yeonee <leenayeon@sookmyung.ac.kr>
2026-01-04 00:47:23 +0900

    setting: #1 아키텍처 기반 폴더링

---
+++ Cherrish-iOS/Cherrish-iOS/Data/Persistence/KeychainService.swift
@@ -0,0 +1,7 @@
+//
+//  KeychainService.swift
+//  Cherrish-iOS
+//
+//  Created by 이나연 on 12/31/25.
+//
+
---
+++ Cherrish-iOS/Cherrish-iOS/Data/Repository/Repository.swift
@@ -0,0 +1,8 @@
+//
+//  Repository.swift
+//  Cherrish-iOS
+//
+//  Created by 이나연 on 1/3/26.
+//
+
+import Foundation
