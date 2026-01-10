ac627c3c77b3828e53610b9a336ac6e2b698a17e
yeonee <leenayeon@sookmyung.ac.kr>
2026-01-11 02:15:10 +0900

    chore: #10 예시코드 추가

---
+++ Cherrish-iOS/Cherrish-iOS/Data/Network/EndPoint/TestAPI.swift
@@ -0,0 +1,16 @@
+//
+//  TestAPI.swift
+//  Cherrish-iOS
+//
+//  Created by 이나연 on 1/11/26.
+//
+
+enum TestAPI {
+    case login
+    case logout
+}
+
+/// extension에서 endpoint를 채택하여 구현
+// extension TestAPI: EndPoint {
+//
+// }
