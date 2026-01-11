26bd26578d2aa7769be2255b109a60520c270145
soseoyo12 <songsy0612@gmail.com>
2026-01-11 23:43:42 +0900

    feat: #36 Color ViewModifier 추가 및 Shadow 디자인 시스템 반영

---
+++ Cherrish-iOS/Cherrish-iOS/Presentation/Global/Extension/View+Shadow.swift
@@ -0,0 +1,24 @@
+//
+//  View+Shadow.swift
+//  Cherrish-iOS
+//
+
+import SwiftUI
+
+struct CherrishShadow: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .shadow(
+                color: Color("shadow"),
+                radius: 10,
+                x: 0,
+                y: 0
+            )
+    }
+}
+
+extension View {
+    func cherrishShadow() -> some View {
+        self.modifier(CherrishShadow())
+    }
+}
