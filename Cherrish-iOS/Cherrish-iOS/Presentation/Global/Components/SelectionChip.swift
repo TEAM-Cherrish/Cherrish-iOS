83afa5be180a0889cb8fc94a340c441b8a7526c2
sum130 <sm3904@naver.com>
2026-01-12 16:18:05 +0900

    refactor: #27 chipComponent 코리 반영

--- Cherrish-iOS/Cherrish-iOS/Presentation/Global/Components/SelectionChip.swift
+++ Cherrish-iOS/Cherrish-iOS/Presentation/Global/Components/SelectionChip.swift
@@ -13,18 +13,15 @@ struct SelectionChip: View {
     @Binding var isSelected: Bool
 
     var body: some View {
-        VStack(alignment: .leading) {
-            Text(title)
-                .typography(.body1_m_14)
-                .foregroundStyle(isSelected ? .gray800 : .gray700)
-        }
+        Text(title)
+            .typography(.body1_m_14)
+            .foregroundStyle(isSelected ? .gray800 : .gray700)
         .frame(width: 148, height: 80)
         .background(isSelected ? .red200 : .gray0)
         .overlay(
             RoundedRectangle(cornerRadius: 10)
                 .stroke(isSelected ? .red500 : .gray500, lineWidth: 1)
         )
-        .cornerRadius(10)
         .onTapGesture {
             isSelected.toggle()
         }
