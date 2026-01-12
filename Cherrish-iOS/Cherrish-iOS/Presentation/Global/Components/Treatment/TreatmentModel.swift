62d5f14266bd1e0389c2fbb8215ccb1f2ef945dd
JaeSunEo <wotjs020708@gmail.com>
2026-01-12 21:08:29 +0900

    feat: #28 TreatmentModel 추가

---
+++ Cherrish-iOS/Cherrish-iOS/Presentation/Global/Components/Treatment/TreatmentModel.swift
@@ -0,0 +1,14 @@
+//
+//  TreatmentModel.swift
+//  Cherrish-iOS
+//
+//  Created by 어재선 on 1/12/26.
+//
+
+import Foundation
+
+struct TreatmentModel {
+    let name: String              
+    let benefits: [String]
+    let downtime: String
+}
