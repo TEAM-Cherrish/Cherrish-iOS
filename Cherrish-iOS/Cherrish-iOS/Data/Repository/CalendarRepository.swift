25c9cdf2afcb612c2300dbd54e5ad4c09defc4ca
yeonee <leenayeon@sookmyung.ac.kr>
2026-01-14 04:36:06 +0900

    feat: #39 오늘 받을 시술 불러오기 유스케이스 작성

--- Cherrish-iOS/Cherrish-iOS/Data/Repository/CalendarRepository.swift
+++ Cherrish-iOS/Cherrish-iOS/Data/Repository/CalendarRepository.swift
@@ -11,6 +11,10 @@ struct DefaultCalendarRepository: CalendarInterface {
     func fetchProcedureCountOfMonth(year: Int, month: Int) -> [Int : Int] {
         return [:]
     }
+    
+    func fetchTodayProcedureList(date: String) -> [ProcedureEntity] {
+        return []
+    }
 }
 
 struct MockCalendarRepository: CalendarInterface {
@@ -21,6 +25,17 @@ struct MockCalendarRepository: CalendarInterface {
             15: 1,
             23: 3,
             31: 6
+        ]
+    }
+    
+    func fetchTodayProcedureList(date: String) -> [ProcedureEntity] {
+        return [
+            ProcedureEntity(title: "레이저 토닝", date: "2026-01-13", downtimeDays: 3),
+            ProcedureEntity(title: "수분 충전 팩하기", date: "2026-01-13", downtimeDays: 5),
+            ProcedureEntity(title: "인모드", date: "2026-01-13", downtimeDays: 6),
+            ProcedureEntity(title: "레이저 토닝", date: "2026-01-13", downtimeDays: 3),
+            ProcedureEntity(title: "수분 충전 팩하기", date: "2026-01-13", downtimeDays: 5),
+            ProcedureEntity(title: "인모드", date: "2026-01-13", downtimeDays: 6)
         ]
     }
 }
