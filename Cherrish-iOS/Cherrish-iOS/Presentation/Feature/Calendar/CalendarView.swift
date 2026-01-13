80034a9894d56cc625d2c1a189512600565d4de0
yeonee <leenayeon@sookmyung.ac.kr>
2026-01-14 01:29:23 +0900

    feat: #39 캘린더 현재 달의 일정 가져오기 + isSelected + 달 변경 구현

--- Cherrish-iOS/Cherrish-iOS/Presentation/Feature/Calendar/CalendarView.swift
+++ Cherrish-iOS/Cherrish-iOS/Presentation/Feature/Calendar/CalendarView.swift
@@ -9,10 +9,79 @@
 import SwiftUI
 
 struct CalendarView: View {
+    @ObservedObject var viewModel: CalendarViewModel
+    let weekdays: [String] = ["일", "월", "화", "수", "목", "금", "토"]
+    let columns = Array(repeating: GridItem(.fixed(40), spacing: 8), count: 7)
+    
     var body: some View {
-        ZStack {
-            Color.red700
-            Text("CalendarView")
+        VStack {
+            calendarHeader
+            dateGridsView
         }
+        .onAppear {
+            viewModel.fetchProcedureCountsOfMonth()
+        }
+    }
+}
+
+extension CalendarView {
+    private var calendarHeader: some View {
+        VStack {
+            HStack {
+                Image(.chevronLeft)
+                    .frame(width: 40, height: 40)
+                    .scaledToFit()
+                    .onTapGesture {
+                        viewModel.currentMonth -= 1
+                        viewModel.selectedDate = viewModel.firstDateOfCurrentMonth()
+                    }
+                
+                Spacer()
+                
+                TypographyText("\(viewModel.getYearAndMonthString())", style: .title2_m_16, color: .gray1000)
+                
+                Spacer()
+                
+                Image(.chevronRight)
+                    .frame(width: 40, height: 40)
+                    .scaledToFit()
+                    .onTapGesture {
+                        viewModel.currentMonth += 1
+                        viewModel.selectedDate = viewModel.firstDateOfCurrentMonth()
+                    }
+            }
+            .padding(.horizontal, 11)
+            .padding(.top, 38)
+            
+            
+            HStack(spacing: 8) {
+                ForEach(weekdays, id: \.self) { weekday in
+                    TypographyText(weekday, style: .body1_r_14, color: .gray800)
+                        .frame(width: 40, height: 40)
+                }
+            }
+            .padding(.horizontal, 23)
+        }
+    }
+    
+    private var dateGridsView: some View {
+        LazyVGrid(columns: columns) {
+            ForEach(viewModel.getDatesArray()) { value in
+                if value.day != -1 {
+                    CalendarCellView(
+                        value: value,
+                        procedureCount: viewModel.getProcedureCount(for: value),
+                        isSelected: viewModel.isSelected(value)
+                    )
+                    .onTapGesture {
+                        viewModel.select(date: value.date)
+                    }
+                } else {
+                    Text("").hidden()
+                }
+            }
+        }
+        .padding(.horizontal, 23)
+        
     }
 }
