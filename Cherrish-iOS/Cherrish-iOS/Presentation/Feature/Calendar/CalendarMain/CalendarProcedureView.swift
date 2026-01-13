44cc400734b6763748b476896f5db346bf412d8b
yeonee <leenayeon@sookmyung.ac.kr>
2026-01-14 04:41:07 +0900

    feat: #39 오늘받을 시술 리스트 스크롤뷰 구현

--- Cherrish-iOS/Cherrish-iOS/Presentation/Feature/Calendar/CalendarMain/CalendarProcedureView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CalendarProcedureView.swift
-//  Cherrish-iOS
-//
-//  Created by 이나연 on 1/14/26.
-//
-
-import Foundation
--- Cherrish-iOS/Cherrish-iOS/Presentation/Feature/Calendar/CalendarMain/CalendarView.swift
+++ Cherrish-iOS/Cherrish-iOS/Presentation/Feature/Calendar/CalendarMain/CalendarView.swift
@@ -17,9 +17,17 @@ struct CalendarView: View {
         VStack {
             calendarHeader
             dateGridsView
+            scheduleListView
         }
         .onAppear {
-            viewModel.fetchProcedureCountsOfMonth()
+            Task {
+                do {
+                    try await viewModel.fetchProcedureCountsOfMonth()
+                    try await viewModel.fetchTodayProcedureList()
+                } catch {
+                    print(error)
+                }
+            }
         }
     }
 }
@@ -83,5 +91,44 @@ extension CalendarView {
         }
         .padding(.horizontal, 23)
         
+    }
+    
+    private var scheduleListView: some View {
+        VStack() {
+            HStack {
+                TypographyText("일정 ・ \(viewModel.procedureList.count)개", style: .body1_r_14, color: .gray1000)
+                Spacer()
+                Image(.plus)
+                    .resizable()
+                    .renderingMode(.template)
+                    .foregroundStyle(.gray600)
+                    .frame(width: 24.adjustedW, height: 24.adjustedH)
+            }
+            .frame(height: 40.adjustedH)
+            .padding(.horizontal, 19)
+            .padding(.top, 8)
+            
+            ScrollView(showsIndicators: false) {
+                ForEach(viewModel.procedureList, id: \.self) { procedure in
+                    ProcedureView(
+                        treatmentTitle: procedure.title,
+                        treatmentDate: procedure.date,
+                        downTimeDays: procedure.downtimeDays
+                    )
+                }
+            }
+            .frame(height: 184.adjustedH)
+            .padding(.top, 6)
+            .padding(.horizontal, 19)
+            .padding(.bottom, 12)
+        }
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.gray0)
+                .cherrishShadow()
+        )
+        .padding(.top, 20)
+        .padding(.horizontal, 25)
+        .padding(.bottom, 18)
     }
 }
