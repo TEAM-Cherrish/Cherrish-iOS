bc49e93fa737053a92c06281336cc75fda07e219
JaeSunEo <wotjs020708@gmail.com>
2026-01-13 22:13:27 +0900

    feat:#29 - AgeTextBox 구현

--- Cherrish-iOS/Cherrish-iOS/Presentation/Global/Components/AgeTextBox.swift
+++ Cherrish-iOS/Cherrish-iOS/Presentation/Global/Components/AgeTextBox.swift
@@ -5,4 +5,24 @@
 //  Created by 어재선 on 1/13/26.
 //
 
-import Foundation
+import SwiftUI
+
+struct AgeTextBox: View {
+    @Binding var text: String
+    let placeholder: String
+
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack(spacing: 0) {
+                TypographyText("나이", style: .body1_sb_14)
+                Spacer()
+            }
+            Spacer()
+                .frame(height: 8)
+            AgeTextField(text: $text, placeholder: placeholder)
+        }
+        
+    }
+    
+}
+
