68e42adb618403b7660d10bc18c4fa62dd27056e
soseoyo12 <songsy0612@gmail.com>
2026-01-14 22:14:34 +0900

    style: #58 코리 반영

--- Cherrish-iOS/Cherrish-iOS/Presentation/Global/Components/OnComingCard.swift
+++ Cherrish-iOS/Cherrish-iOS/Presentation/Global/Components/OnComingCard.swift
@@ -13,37 +13,27 @@ struct OnComingCard: View {
     let dDay: Int
     var onTap: (() -> Void)? = nil
     
-    private var formattedDate: String {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd"
-        
-        guard let parsedDate = formatter.date(from: date) else {
-            return date
-        }
-        
-        formatter.dateFormat = "M월 d일"
-        return formatter.string(from: parsedDate)
-    }
-    
     var body: some View {
         HStack(spacing: 0) {
             VStack(alignment: .leading, spacing: 2.adjustedH) {
-                TypographyText(formattedDate, style: .body1_m_14, color: Color("gray_700"))
+                TypographyText(date.toKoreanMonthDay, style: .body1_m_14, color: .gray700)
                     .frame(height: 20.adjustedH)
                 
-                TypographyText(name, style: .title2_m_16, color: Color("gray_900"))
+                TypographyText(name, style: .title2_m_16, color: .gray900)
                     .frame(height: 24.adjustedH)
             }
             
             Spacer()
             
             HStack(spacing: 12.adjustedW) {
-                TypographyText("D-\(dDay)", style: .body3_r_12, color: Color("gray_600"))
+                TypographyText("D-\(dDay)", style: .body3_r_12, color: .gray600)
                     .frame(width: 40.adjustedW, height: 18.adjustedH)
                     .background(Color("gray_200"))
                     .cornerRadius(20.adjustedW)
                 
-                Image("chevron_left_gray")
+                Image(.chevronRight)
+                    .renderingMode(.template)
+                    .gray700()
 
             }
             .padding(.vertical, 1.adjustedH)
