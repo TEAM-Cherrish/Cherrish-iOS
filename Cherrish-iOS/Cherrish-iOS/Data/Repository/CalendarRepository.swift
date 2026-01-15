7e4c6d5a53a0cce4fa1c34b3dd01e95648190b99
yeonee <leenayeon@sookmyung.ac.kr>
2026-01-15 23:35:31 +0900

    chore: #39 목데이터 수정

--- Cherrish-iOS/Cherrish-iOS/Data/Repository/CalendarRepository.swift
+++ Cherrish-iOS/Cherrish-iOS/Data/Repository/CalendarRepository.swift
@@ -30,12 +30,91 @@ struct MockCalendarRepository: CalendarInterface {
     
     func fetchTodayProcedureList(date: String) -> [ProcedureEntity] {
         return [
-            ProcedureEntity(title: "레이저 토닝", date: "2026-01-13", downtimeDays: 3),
-            ProcedureEntity(title: "수분 충전 팩하기", date: "2026-01-13", downtimeDays: 5),
-            ProcedureEntity(title: "인모드", date: "2026-01-13", downtimeDays: 6),
-            ProcedureEntity(title: "레이저 토닝", date: "2026-01-13", downtimeDays: 3),
-            ProcedureEntity(title: "수분 충전 팩하기", date: "2026-01-13", downtimeDays: 5),
-            ProcedureEntity(title: "인모드", date: "2026-01-13", downtimeDays: 6)
+            ProcedureEntity(
+                procedureId: 1,
+                name: "레이저 토닝",
+                downtimeDays: "7",
+                sensitiveDays: [
+                    "2026-01-15",
+                    "2026-01-16",
+                    "2026-01-17"
+                ],
+                cautionDays: [
+                    "2026-01-18",
+                    "2026-01-19"
+                ],
+                recoveryDays: [
+                    "2026-01-20",
+                    "2026-01-21"
+                ]
+            ),
+            ProcedureEntity(
+                procedureId: 2,
+                name: "보톡스",
+                downtimeDays: "3",
+                sensitiveDays: [
+                    "2026-01-15"
+                ],
+                cautionDays: [
+                    "2026-01-16"
+                ],
+                recoveryDays: [
+                    "2026-01-17"
+                ]
+            ),
+            ProcedureEntity(
+                procedureId: 3,
+                name: "필러",
+                downtimeDays: "5",
+                sensitiveDays: [
+                    "2026-01-15",
+                    "2026-01-16"
+                ],
+                cautionDays: [
+                    "2026-01-17",
+                    "2026-01-18"
+                ],
+                recoveryDays: [
+                    "2026-01-19"
+                ]
+            ),
+            ProcedureEntity(
+                procedureId: 4,
+                name: "IPL 레이저",
+                downtimeDays: "2",
+                sensitiveDays: [
+                    "2026-01-15"
+                ],
+                cautionDays: [
+                    "2026-01-16"
+                ],
+                recoveryDays: []
+            ),
+            ProcedureEntity(
+                procedureId: 5,
+                name: "윤곽 주사",
+                downtimeDays: "4",
+                sensitiveDays: [
+                    "2026-01-15",
+                    "2026-01-16"
+                ],
+                cautionDays: [
+                    "2026-01-17"
+                ],
+                recoveryDays: [
+                    "2026-01-18"
+                ]
+            ),
+            ProcedureEntity(
+                procedureId: 6,
+                name: "피부 스케일링",
+                downtimeDays: "1",
+                sensitiveDays: [
+                    "2026-01-15"
+                ],
+                cautionDays: [],
+                recoveryDays: []
+            )
         ]
     }
 }
