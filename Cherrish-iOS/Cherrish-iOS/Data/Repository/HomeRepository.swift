d74194c85ed637f5d63f1ca20328dc82c9c7b63c
soseoyo12 <songsy0612@gmail.com>
2026-01-18 19:00:40 +0900

    feat: #49 실제 api 연결란 목 데이터 삭제

--- Cherrish-iOS/Cherrish-iOS/Data/Repository/HomeRepository.swift
+++ Cherrish-iOS/Cherrish-iOS/Data/Repository/HomeRepository.swift
@@ -15,57 +15,9 @@ struct DefaultHomeRepository: HomeInterface {
     }
     
     func fetchDashboard() async throws -> DashboardEntity {
-        return createMockDashboard()
+        fatalError("Not implemented")
     }
-    
-    private func createMockDashboard() -> DashboardEntity {
-        DashboardEntity(
-            date: "2026-01-15",
-            dayOfWeek: "WEDNESDAY",
-            challengeName: "피부컨디션 챌린지",
-            cherryLevel: 4,
-            challengeRate: 80.1,
-            recentProcedures: [
-                RecentProcedureEntity(
-                    name: "레이저 토닝",
-                    daysSince: 3,
-                    currentPhase: .sensitive
-                ),
-                RecentProcedureEntity(
-                    name: "보습 케어",
-                    daysSince: 5,
-                    currentPhase: .caution
-                ),
-                RecentProcedureEntity(
-                    name: "필러",
-                    daysSince: 8,
-                    currentPhase: .recovery
-                )
-            ],
-            upcomingProcedures: [
-                UpcomingProcedureEntity(
-                    date: "2026-01-20",
-                    name: "보톡스",
-                    count: 2,
-                    dDay: 5
-                ),
-                UpcomingProcedureEntity(
-                    date: "2026-01-25",
-                    name: "필러",
-                    count: 1,
-                    dDay: 10
-                ),
-                UpcomingProcedureEntity(
-                    date: "2026-01-25",
-                    name: "필러",
-                    count: 1,
-                    dDay: 10
-                )
-            ]
-        )
-    }
-    
-    
+
 }
 
 struct MockHomeRepository: HomeInterface {
