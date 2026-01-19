f2a38db4db356cb356dfefdf78936316e14bbc7c
JaeSunEo <wotjs020708@gmail.com>
2026-01-19 18:24:19 +0900

    chore: #103 줄 바꿈

--- Cherrish-iOS/Cherrish-iOS/Presentation/Feature/Calendar/Treatment/Treatment/ViewModel/TreatmentViewModel.swift
+++ Cherrish-iOS/Cherrish-iOS/Presentation/Feature/Calendar/Treatment/Treatment/ViewModel/TreatmentViewModel.swift
@@ -19,8 +19,6 @@ final class TreatmentViewModel: ObservableObject{
     @Published var searchText = ""
     @Published var filteredTreatments: [TreatmentEntity] = []
     
-    
-    
     var step: Int { state.rawValue }
     
     var cancellables = Set<AnyCancellable>()
