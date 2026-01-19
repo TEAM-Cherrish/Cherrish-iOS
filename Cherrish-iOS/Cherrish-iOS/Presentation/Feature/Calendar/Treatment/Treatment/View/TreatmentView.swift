f24147edbcf03001dbd575a19ccf7d17f322baa8
JaeSunEo <wotjs020708@gmail.com>
2026-01-19 18:01:52 +0900

    fix: #103 안쓰는 코드 삭제

--- Cherrish-iOS/Cherrish-iOS/Presentation/Feature/Calendar/Treatment/Treatment/View/TreatmentView.swift
+++ Cherrish-iOS/Cherrish-iOS/Presentation/Feature/Calendar/Treatment/Treatment/View/TreatmentView.swift
@@ -73,63 +73,6 @@ struct TreatmentView: View {
             .id(viewModel.step)
         }
         .ignoresSafeArea(.keyboard)
-    }
-}
-
-//MARK: - TreatmentSelectedCategoryView
-
-private struct TreatmentSelectedCategory: View {
-    @ObservedObject var viewModel: NoTreatmentViewModel
-    let columns = [
-        GridItem(.flexible()),
-        GridItem(.flexible())
-    ]
-    var body: some View {
-        VStack {
-            Spacer()
-                .frame(height: 70.adjustedH)
-            HStack(spacing: 0) {
-                VStack(alignment: .leading, spacing: 0) {
-                    TypographyText(
-                        "요즘 가장 신경 쓰이는 ",
-                        style: .title1_sb_18,
-                        color: .gray1000
-                    )
-                    TypographyText(
-                        "피부 고민은 무엇인가요?",
-                        style: .title1_sb_18,
-                        color: .gray1000
-                    )
-                    TypographyText(
-                        "선택한 고민을 기준으로 시술 정보를 정리해줘요.",
-                        style: .body1_m_14,
-                        color: .gray700
-                    )
-                }
-                Spacer()
-            }
-            Spacer()
-                .frame(height: 40.adjustedH)
-            LazyVGrid(columns: columns, spacing: 12) {
-                ForEach(TreatmentCategory.allCases, id: \.id) { catagory in
-                    SelectionChip(
-                        title: catagory.title,
-                        isSelected: Binding(
-                            get: {
-                                viewModel.treatmentCatagory == catagory
-                            },
-                            set: {
-                                isSelected in
-                                guard isSelected else {
-                                    return
-                                }
-                                viewModel.treatmentCatagory = catagory
-                            }
-                        )
-                    )
-                    
-                }
-            }
-        }
+        
     }
 }
