bd08d7a61bb696b458ebaff4a5cce1d26379f3b3
JaeSunEo <wotjs020708@gmail.com>
2026-01-19 18:05:46 +0900

    fix: #103 mainactor 추가

--- Cherrish-iOS/Cherrish-iOS/Presentation/Feature/Calendar/Treatment/NoTreatment/ViewModel/NoTreatmentViewModel.swift
+++ Cherrish-iOS/Cherrish-iOS/Presentation/Feature/Calendar/Treatment/NoTreatment/ViewModel/NoTreatmentViewModel.swift
@@ -47,6 +47,7 @@ final class NoTreatmentViewModel: ObservableObject{
         state.previous()
     }
     
+    @MainActor
     func loadCategories() async {
             do {
                 categories = try await fetchCategoriesUseCase.execute()
