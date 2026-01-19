e94afa32acd3ccdcc61a271c655ff90ed7d1356b
JaeSunEo <wotjs020708@gmail.com>
2026-01-19 16:15:15 +0900

    chore: #103 파일 이름 수정

--- Cherrish-iOS/Cherrish-iOS/Presentation/Feature/Calendar/Treatment/NoTreatment/View/NoTreatmentView.swift
+++ Cherrish-iOS/Cherrish-iOS/Presentation/Feature/Calendar/Treatment/NoTreatment/View/NoTreatmentView.swift
@@ -84,7 +84,7 @@ struct NoTreatmentView: View {
     private func bottomView() -> some View {
         VStack(spacing: 0) {
             if viewModel.state == .treatmentFilter, !viewModel.selectedTreatments.isEmpty {
-                SelectedTreatmentView(
+                SelectedTreatmentSheetView(
                     selectedTreatments: viewModel.selectedTreatments,
                     removeTreatment: viewModel.removeTreatment(_:)
                 )
