508de8b3fc4be167f41ad1eb852078d579d2b34c
sum130 <sm3904@naver.com>
2026-01-21 03:03:17 +0900

    feat: #91 엔티티

--- Cherrish-iOS/Cherrish-iOS/Domain/Model/ChallengeMissionEntity.swift
+++ Cherrish-iOS/Cherrish-iOS/Domain/Model/ChallengeMissionEntity.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct ChallengeMissionEntity: Identifiable, Equatable {
-    let id: UUID = UUID()
+struct ChallengeMissionEntity: Identifiable, Equatable, Hashable {
+    let id: Int
     let title: String
 }
