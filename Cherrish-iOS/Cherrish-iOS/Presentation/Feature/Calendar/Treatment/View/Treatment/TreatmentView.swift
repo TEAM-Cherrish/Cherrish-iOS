da99026c9a89b367e9011537d433a665ea5eef83
yeonee <leenayeon@sookmyung.ac.kr>
2026-01-22 18:37:59 +0900

    style: #157 네비바 상단 spacer 추가

--- Cherrish-iOS/Cherrish-iOS/Presentation/Feature/Calendar/Treatment/View/Treatment/TreatmentView.swift
+++ Cherrish-iOS/Cherrish-iOS/Presentation/Feature/Calendar/Treatment/View/Treatment/TreatmentView.swift
@@ -14,6 +14,9 @@ struct TreatmentView: View {
 
     var body: some View {
         VStack(spacing: 0) {
+            Spacer()
+                .frame(height: 20.adjustedH)
+            
             CherrishNavigationBar(
                 title: viewModel.state.title,
                 leftButtonAction: {
